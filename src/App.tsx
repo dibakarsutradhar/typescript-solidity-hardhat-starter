b7ba6405b4e09e5f5702265af514f9af556cf5f7
Dibakar Sutra Dhar <iamdibakardipu@gmail.com>
2021-08-12 23:14:54 +0600

    hardhat config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 
 function App() {
   return (
     <div className="App">
       <h1>Solidity HardHat Ethers DApp Template with Typescript</h1>
+      {console.log("Greeter ABI: ", Greeter.abi)}
     </div>
   );
 }
